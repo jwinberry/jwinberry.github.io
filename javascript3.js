830e5db709be9401415a7e4338e0ff8d131e13e5
jwinberry <jwinberry3299@gmail.com>
2017-12-06 20:51:32 -0800

    mod of javascript3.js

--- javascript3.js
+++ javascript3.js
@@ -20,16 +20,3 @@ function draw() {
 
 
 
-function draw() {
-    if (mouseIsPressed) {
-        colorMode(HSB, 300, 100, 100);
-        fill(random(255), random(255), random(255), random(200));
-        
-        
-    } else {
-        colorMode(RGB);
-        fill(random(255), random(255), random(255), random(200));
-        
-    }
-    ellipse(random(windowWidth), random(windowHeight), random(150), random(175));
-}
